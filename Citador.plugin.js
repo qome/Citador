6c115e17572610cd3d729d3796fc07b374c78a5d
Eduardo Londero <eduardolonderovieira@gmail.com>
2019-04-29 21:02:53 -0300

    bump version

--- Citador.plugin.js
+++ Citador.plugin.js
@@ -27,7 +27,7 @@ var Citador = (() => {
   
   getName         () { return "Citador";            }
   getDescription  () { return this.local.description}
-  getVersion      () { return "1.7.19";             }
+  getVersion      () { return "1.7.20";             }
   getAuthor       () { return "Nirewen";            }
   unload          () { this.deleteEverything();     }
   stop            () { this.deleteEverything();     }
